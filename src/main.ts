cdaa882dfa7347cf43c13b05abd34c0ac0062960
Tom Sternberg <tomc-s@comcast.net>
2023-07-28 11:02:22 -0700

    Updated Libraries

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-module.exports={
-  addons: ['storybook-addon-designs']
-}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
